d1cb25b3b23c6b6cc80e3636f681d87b93327649
Myrmex <dfusi@hotmail.com>
2024-07-26 18:35:57 +0200

    Update PushLocal.bat

--- PushLocal.bat
+++ PushLocal.bat
@@ -3,5 +3,5 @@ echo PRESS ANY KEY TO INSTALL TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
 c:\exe\nuget add .\Cadmus.Pura.Parts\bin\Debug\Cadmus.Pura.Parts.8.0.4.nupkg -source C:\Projects\_NuGet
 c:\exe\nuget add .\Cadmus.Pura.Services\bin\Debug\Cadmus.Pura.Services.8.0.4.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Seed.Pura.Parts\bin\Debug\Cadmus.Seed.Pura.Parts.8.0.4.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Seed.Pura.Parts\bin\Debug\Cadmus.Seed.Pura.Parts.8.0.5.nupkg -source C:\Projects\_NuGet
 pause
